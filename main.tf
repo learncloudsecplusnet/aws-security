d966d3580c85425a1efc4e673fb9e62d377d6b1f
unknown <csa2@learncloudsecplus.net name = Ope Omotayo>
2019-08-20 11:30:07 +0100

    added ssh ingress ip

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_security_group" "aws-security" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["80.194.39.211/32"]
   }
 
   # HTTP access from the VPC
